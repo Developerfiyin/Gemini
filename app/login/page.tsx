f2a88dbad6b615c4fe2f3152364e39a3f2158339
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-03-09 00:01:34 +0100

    Updated main page dashboard

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,10 @@
 import Image from "next/image"
 import { LoginForm } from "@/components/login-form"
-import smage from '@/components/images/Fiyinfoluwa.jpeg'
 export default function LoginPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-4xl">
         <LoginForm />
-        <img src={smage.src}  alt="" />
-        
       </div>
     </div>
   )
