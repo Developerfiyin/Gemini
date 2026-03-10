40fad8d81b67aaf5487841ef6b597f4c06bd8f96
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-03-10 20:15:22 +0100

    Install react router dom

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -1,21 +1,27 @@
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import userImage from '@/components/images/user1.jpg'
+"use client";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import userImage from "@/components/images/user1.jpg";
 
-import { Card, CardContent } from "@/components/ui/card"
+import { Card, CardContent } from "@/components/ui/card";
 import {
   Field,
   FieldDescription,
   FieldGroup,
   FieldLabel,
   FieldSeparator,
-} from "@/components/ui/field"
-import { Input } from "@/components/ui/input"
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+
+import Link from "next/link";
+
+import { useRouter } from "next/navigation";
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const router = useRouter();
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className="overflow-hidden p-0">
@@ -25,7 +31,7 @@ export function LoginForm({
               <div className="flex flex-col items-center gap-2 text-center">
                 <h1 className="text-2xl font-bold">Welcome back</h1>
                 <p className="text-balance text-muted-foreground">
-                  Login to your 
+                  Login to your account
                 </p>
               </div>
               <Field>
@@ -50,7 +56,7 @@ export function LoginForm({
                 <Input id="password" type="password" required />
               </Field>
               <Field>
-                <Button type="submit">Login</Button>
+                <Button onClick={() => router.push("/dashboard")}>Login</Button>
               </Field>
               <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
                 Or continue with
@@ -91,7 +97,7 @@ export function LoginForm({
           </form>
           <div className="relative hidden bg-muted md:block">
             <img
-        src={userImage.src}  
+              src={userImage.src}
               alt="Image"
               className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
             />
@@ -103,5 +109,5 @@ export function LoginForm({
         and <a href="#">Privacy Policy</a>.
       </FieldDescription>
     </div>
-  )
+  );
 }
